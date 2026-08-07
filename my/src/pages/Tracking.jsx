302ef5fd944c76f5aac3158e0aae6eb29da30143
dominicagwu <ukadominic9@gmail.com>
2026-08-07 12:05:20 +0100

    fix Supabase import and tracking page

--- my/src/pages/Tracking.jsx
+++ my/src/pages/Tracking.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import { supabase } from "../supabase";
+import { supabase } from "../lib/supabase";
 
 function Tracking() {
   const [trackingNumber, setTrackingNumber] = useState("");
